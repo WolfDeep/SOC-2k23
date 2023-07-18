aa8636c1d7353f5a070a9726db3667db51fb4b5b
Deepika Jandu <104681486+WolfDeep@users.noreply.github.com>
2023-07-18 19:38:36 +0530

    Add files via upload

---
+++ Cricbuzz/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const cors = require("cors");
+
+app.use(express.json()) //req. body
+app.use(cors())
+
+// ROUTES//
+app.listen(5000, ()=>{
+    console.log("Server is running")
+})
